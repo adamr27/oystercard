6df890dc3a6c8e58c0c5705f128a2541a570c6de
adam robinson <adam.james.robinson@hotmail.co.uk>
2019-10-07 15:39:40 +0100

    completed step 8

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -4,11 +4,27 @@ class Oystercard
   CARD_LIMIT=90
 
   def initialize
-    @balance = 0
+    @balance, @travelling = 0, false
+
   end
 
   def top_up(money)
     fail 'over the limit' if @balance + money > CARD_LIMIT else @balance += money
   end
 
+  def deduct(money)
+    @balance -= money
+  end
+
+  def in_journey?
+    @travelling
+  end
+
+  def touch_in
+    @travelling = true
+  end
+
+  def touch_out
+    @travelling = false
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -12,4 +12,30 @@ describe Oystercard do
   it 'cannot add more than 90' do
     expect{ subject.top_up(100) }.to raise_error('over the limit')
   end
-end
+
+  it 'cannot add more to maxed balance' do
+    max_balance = Oystercard::CARD_LIMIT
+    subject.top_up(max_balance)
+    expect{subject.top_up(1)}.to raise_error('over the limit')
+  end
+
+  it "can take money off the card" do
+    subject.top_up(20)
+    expect(subject.deduct(10)).to eq(10)
+  end
+
+  it "can check if in journey?" do
+    expect(subject.in_journey?).to eq(false)
+  end
+
+  it "can be touched in" do
+    subject.touch_in
+    expect(subject.in_journey?).to eq(true)
+  end
+
+  it "can touch back out" do
+    subject.touch_in
+    subject.touch_out
+    expect(subject.in_journey?).to eq(false)
+  end
+end 
