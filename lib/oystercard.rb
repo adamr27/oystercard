34400c725aed6c92afa2648fe85826d19b188fc4
albie-b <55197619+albie-b@users.noreply.github.com>
2019-10-07 14:42:09 +0100

    completed step 5

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,8 +1,13 @@
 class Oystercard
+
   attr_reader :balance
 
   def initialize
     @balance = 0
   end
-  
+
+  def top_up(money)
+    @balance += money
+  end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,8 @@ describe Oystercard do
   it 'has a balance of 0 when started' do
     expect(subject.balance).to eq(0)
   end
+
+  it 'can add money to my card' do
+    expect(subject.top_up(10.10)).to eq(subject.balance)
+  end
 end
